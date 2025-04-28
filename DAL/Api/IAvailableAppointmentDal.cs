85d17cff7f90ad748ebd5212f8b2df298ca29c64
YehuditOutmazgin <Hand3D3@mbjcomp.org.il>
2025-04-28 16:23:45 +0300

    Update IAvailableAppointmentDal.cs

--- DAL/Api/IAvailableAppointmentDal.cs
+++ DAL/Api/IAvailableAppointmentDal.cs
@@ -8,5 +8,15 @@ namespace DAL.Api
 {
     internal interface IAvailableAppointmentDal
     {
+        // add available appointment 
+        Task AddAppointment(AvailableAppointment appointment);
+        // get all available appointments for a specific therapist and date its return a list of available appointments for this week.
+        Task<List<AvailableAppointment>> GetAppointmentsByTherapistAndDate(DateOnly date, int therapistId);
+        // get all available appointments for a specific date and specialization its return a list of available appointments for this week.
+        Task<List<AvailableAppointment>> GetAppointmentsBySpecializationAndDate(DateOnly date, int specialization);
+        Task RemoveAppointment(int appointmentId);
+        Task RemoveAllAppointmentsByDate(DateOnly date);
+        Task RemoveAllAppointmentsByDateAndTherapist(int therapistId, DateOnly date);
+        Task<AvailableAppointment> GetAppointmentByTherapistAndDate(int therapistId, DateTime date);
     }
 }
